87ea2f5d3788696082caf288ec9d1fe5b7a62439
NguyenMinhNhat82 <20510520925nhat@ou.edu.vn>
2023-07-06 06:26:56 -0700

    aaa

--- orderFoodAPI/src/main/java/com/example/orderfoodapi/repository/UserUuDaiRepository.java
+++ orderFoodAPI/src/main/java/com/example/orderfoodapi/repository/UserUuDaiRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserUuDaiRepository extends JpaRepository<UserUudai,Integer> {
+    UserUudai findUserUudaiById(int id);
 }
